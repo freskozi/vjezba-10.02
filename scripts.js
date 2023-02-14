2dea6b14cd24b4e832757a7c354ae51c0fe41c30
freskozi <denisbandic98@gmail.com>
2023-02-14 19:08:47 +0100

    aq

---
+++ scripts.js
@@ -0,0 +1,20 @@
+var firstName, lastName, price, discount, fullPrice;
+firstName = "John";
+lastName = "Doe";
+price = 19.90;
+discount = 0.10;
+var x = 1;
+if (x == 1){
+x = 2;
+}
+ 
+var a = true;
+if (a == true) {
+      var z = 44;
+      return 0;
+  }
+if (discount !=0) {
+  fullPrice = price * 100 / discount;
+} else {
+    fullPrice = price;
+}
